4c73b293418875b73d1f5a2de3d71b5571cceed0
aleanrocha <ale.oficial.me@gmail.com>
2023-10-08 16:51:22 -0300

    review submission component

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,6 +1,27 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiFrownFill,
+  BsFillEmojiNeutralFill,
+} from 'react-icons/bs'
+import './Thanks.css'
 const Thanks = () => {
   return (
-    <div><h2>Thanks</h2></div>
+    <div className='thanks-container'>
+      <h2>Falta pouco...</h2>
+      <p>
+        Asua opinião é muito importante, em breve você receberá um cupom de
+        desconto para sua próxima compra.
+      </p>
+      <p>Para concluir sua avaliação clique no botão de enviar abaixo.</p>
+      <h3>Aqui está o resumo de sua avaliação</h3>
+      <p className='review-data'>
+        <span>Satisfação com o produto:</span>
+      </p>
+      <p className='review-data'>
+        <span>Comentário:</span>
+      </p>
+    </div>
   )
 }
 
